568bfb60f55b03633ecc59d1f66941ddfdab8a89
MissDine <enidnyatichi1212@gmail.com>
2022-05-12 23:04:56 +0300

    Feat:add

--- src/App.js
+++ src/App.js
@@ -12,14 +12,16 @@ const App = () => {
 	const [favourites, setFavourites] = useState([]);
 	const [searchValue, setSearchValue] = useState('');
 
+
 	const getMovieRequest = async (searchValue) => {
-		const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=263d22d8`;
+		const url = `http://www.omdbapi.com/?t=${searchValue}&apikey=4599ed4c`;
 
 		const response = await fetch(url);
 		const responseJson = await response.json();
+		console.log(url)
 
-		if (responseJson.Search) {
-			setMovies(responseJson.Search);
+		if (responseJson.Title) {
+			setMovies(preMovies => [...preMovies, responseJson]);
 		}
 	};
 
